b3a03c1381e42e13de83520f6c7c535a8a66885e
Skrukow245 <skrukow245@gmail.com>
2021-05-11 10:58:10 -0600

    Update main.js

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -2,9 +2,10 @@ function Function1()   {
     var str = "This text is red";         //settings variable
     var result = str.fontcolor("red");    //changing the color of the variable to red
     document.getElementById("Red_Text").innerHTML = result;           //linking this to the id
+}
 
 function myFunction()   {
     var sentence = "I am learning";           //settings variable
     sentence += " a lot from this book!";     //combining the 2 strings together
-    document.getElementById("Concatenate").innerHTML = sentence;     //linking this to the id
+    document.getElementById("Concatenate").innerHTML = sentence;
 }
