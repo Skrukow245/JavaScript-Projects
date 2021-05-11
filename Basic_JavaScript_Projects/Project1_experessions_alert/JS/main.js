12977d22de07fd5ea7984706611aeae753cf465a
Skrukow245 <skrukow245@gmail.com>
2021-05-11 09:38:57 -0600

    update

---
+++ Basic_JavaScript_Projects/Project1_experessions_alert/JS/main.js
@@ -0,0 +1,7 @@
+var A = "\"This is a string\"", B = "This is a different string", C = "colored text string"   //setting variables as strings
+var C = C.fontcolor("red")  //Coloring string C to red
+document.write(C)        //writing string C
+window.alert(A)         //Window pop up alert of string A
+document.write(A)      //Writing out string A
+document.write(A + B)  //combining A and B strings into 1
+document.write(2+2)  //expression doing 2+2
